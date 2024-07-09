1c7c504fe5e307d69def145d13b252007f597d02
kunal12kumar <23cs3035@rgipt.ac.in>
2024-07-10 00:08:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './App.css';
+import { faFaceAngry } from '@fortawesome/free-regular-svg-icons/faFaceAngry';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I am kunal</h1>
+      <FontAwesomeIcon icon={faFaceAngry}></FontAwesomeIcon>
+
+    </div>
+  );
+}
+
+export default App;
